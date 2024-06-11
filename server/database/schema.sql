f30f61ee82ac93e4bdcd237248e86b7bc9235280
Florine <florinevinet@gmail.com>
2024-06-11 15:02:59 +0200

    schema changed and works

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -15,7 +15,8 @@ create table recipe (
   duration time null,
   people_number int not null,
   step_description text not null,
-  foreign key(user_id) references user(id) int not null
+  user_id int not null,
+  foreign key(user_id) references user(id)
 );
 
 create table ingredient (
@@ -34,34 +35,44 @@ create table tool (
   name varchar(50) not null
 );
 
-create table recipe_favorite(
-  foreign key(user_id) references user(id) int not null,
-  foreign key(recipe_id) references recipe(id) int not null,
+create table add_favorite(
+  user_id int not null,
+  foreign key(user_id) references user(id),
+  recipe_id int not null,
+  foreign key(recipe_id) references recipe(id)
 );
 
-create table recipe_ingredient(
-  foreign key(ingredient_id) references ingredient(id) int not null,
-  foreign key(recipe_id) references recipe(id) int not null,
+create table add_ingredient(
+  ingredient_id int not null,
+  foreign key(ingredient_id) references ingredient(id),
+  recipe_id int not null,
+  foreign key(recipe_id) references recipe(id),
   quantity int not null,
   unit varchar(5) not null
 );
 
-create table recipe_category(
-  foreign key(recipe_id) references recipe(id) int not null,
-  foreign key(category_id) references category(id) int not null
+create table add_category(
+  recipe_id int not null,
+  foreign key(recipe_id) references recipe(id),
+  category_id int not null,
+  foreign key(category_id) references category(id)
 );
 
-create table comment(
+create table add_comment(
   id int primary key auto_increment not null,
   title varchar(80) not null,
   description text not null,
   rate int(5) not null,
-  foreign key(user_id) references user(id) int not null,
-  foreign key(recipe_id) references recipe(id) int not null
+  user_id int not null,
+  foreign key(user_id) references user(id),
+  recipe_id int not null,
+  foreign key(recipe_id) references recipe(id)
 );
 
-create table recipe_tool(
-  foreign key(recipe_id) references recipe(id) int not null,
-  foreign key(tool_id) references tool(id) int not null,
+create table add_tool(
+  recipe_id int not null,
+  foreign key(recipe_id) references recipe(id),
+  tool_id int not null,
+  foreign key(tool_id) references tool(id),
   quantity int not null
 );
