fe8089d0f65d50ba496321808f81aa60d71c68dc
Micka972 <dem972md@gmail.com>
2024-06-13 17:29:19 +0200

    us2 welcomepage beginning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
-import "./styles/global.css"
+import "./styles/global.css";
 
 function App() {
   return (
     <>
-    <h1>coucou c'est nous</h1>
-    <Outlet/>
+      <h1>coucou c'est nous</h1>
+      <Outlet />
     </>
   );
 }
--- client/src/pages/WelcomePage.jsx
+++ client/src/pages/WelcomePage.jsx
@@ -1,10 +1,19 @@
-function WelcomePage (){
-    return (
-        <>
-        <h1>Page de bienvenue</h1>
-        <h1>Page de bienvenue</h1>
-        </>
-    )
+import "../styles/pages-styles/welcome-page.css";
+
+function WelcomePage() {
+  return (
+    <>
+      <img
+        src="./src/assets/logo_popote_titre.png"
+        alt="logo titre"
+        className="welcomePage-img-logotitre"
+      />
+      <h1>Viens t'inspirer avec les meilleurs recettes du moment</h1>
+      <img src="./src/assets/logo_popote.png" alt="logo" />
+      {/* <Link to="/HomePage">Jeter un coup d'oeil</Link> */}
+      {/* <Link to="/ConnexionPage">Connexion</Link> */}
+    </>
+  );
 }
 
 export default WelcomePage;
