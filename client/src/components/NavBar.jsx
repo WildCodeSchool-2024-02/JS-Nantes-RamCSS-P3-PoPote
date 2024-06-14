a187426ab8f7ff5f1f6abed364f4dc81c9be68fa
TheoDegorre <degorret@gmail.com>
2024-06-14 12:39:13 +0200

    feature in construction desktop part finished mobile part comming

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,44 +1,32 @@
+import { NavLink } from "react-router-dom";
 import LogoPopote from "../assets/logo/logo_popote_last.png";
+import home from "../assets/home_icone.svg";
+import add from "../assets/add_icone.svg";
+import favorite from "../assets/favorite_icone.svg";
+import profile from "../assets/profile_icone.svg";
 
 function NavBar() {
   return (
-    <nav className="navbar-desktop navbar-mobile">
-      <a className="desktop-link" href="/" aria-label="Page d'acceuil">
-        <img src={LogoPopote} alt="logo Popote" />
-      </a>
-      <a
-        className="desktop-link"
-        href="/search"
-        aria-label="Page Recherche"
-      >
-        Rechercher
-      </a>
-      <a
-        className="desktop-link"
-        href="/favorites"
-        aria-label="Page Mes favoris"
-      >
-        Mes favoris
-      </a>
-      <a
-        className="desktop-link"
-        href="/creation-recipe"
-        aria-label="Page Creation de recette"
-      >
-        {" "}
-        Créer une recette
-      </a>
-      <a
-        className="desktop-link"
-        href="/profile"
-        aria-label="Page Mon Profil"
-      >
-        Mon Profil
-      </a>
-
-      <a className="mobile-navbar" href="/" aria-label="Page d'acceuil" >
-      HOME
-      </a>
+    <nav className="navbar">
+      <NavLink to="/" aria-label="Page d'acceuil">
+        <img className="desktop-link" src={LogoPopote} alt="logo Popote" />
+        <img className="mobile-link" src={home} alt="" />
+      </NavLink>
+      <NavLink to="/search" aria-label="Page Recherche">
+        <h1 className="desktop-link">Rechercher</h1>
+      </NavLink>
+      <NavLink to="/favorites" aria-label="Page Mes favoris">
+        <h1 className="desktop-link">Mes Favoris</h1>
+        <img className="mobile-link" src={favorite} alt="" />
+      </NavLink>
+      <NavLink to="/creation-recipe" aria-label="Page Creation de recette">
+        <h1 className="desktop-link">Créer une recette</h1>
+        <img className="mobile-linky" src={add} alt="" />
+      </NavLink>
+      <NavLink to="/profile" aria-label="Page Mon Profil">
+        <h1 className="desktop-link">Mon Profil</h1>
+        <img className="mobile-link" src={profile} alt="" />
+      </NavLink>
     </nav>
   );
 }
