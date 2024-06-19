46c190941d71cfae83f242727d6b412e6f868e07
TheoDegorre <degorret@gmail.com>
2024-06-19 12:32:59 +0200

    navbar feature finished

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,31 +1,86 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import LogoPopote from "../assets/logo/logo_popote_last.png";
+
 import home from "../assets/home_icone.svg";
+import homegreen from "../assets/home_green_icone.svg";
+
+import search from "../assets/search_icone.svg";
+import searchgreen from "../assets/search_green_icone.svg";
+
 import add from "../assets/add_icone.svg";
+import addgreen from "../assets/add_green_icone.svg";
+
 import favorite from "../assets/favorite_icone.svg";
+import favoritegreen from "../assets/favorite_green_icone.svg";
+
 import profile from "../assets/profile_icone.svg";
+import profilegreen from "../assets/profile_green_icone.svg";
+
+import navBg from "../assets/navbar_bg.svg";
 
 function NavBar() {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
-      <NavLink to="/" aria-label="Page d'acceuil">
-        <img className="desktop-link" src={LogoPopote} alt="logo Popote" />
-        <img className="mobile-link" src={home} alt="" />
+      <img src={navBg} className="mobile-link nav-bg" alt="" />
+      <NavLink className="nav-link" to="/" aria-label="Page d'acceuil">
+        <img
+          className="desktop-link"
+          id="popote-logo"
+          src={LogoPopote}
+          alt="logo Popote"
+        />
+        <img
+          className="mobile-link"
+          src={location.pathname === "/" ? homegreen : home}
+          alt=""
+        />
       </NavLink>
-      <NavLink to="/search" aria-label="Page Recherche">
+      <NavLink
+        className="nav-link"
+        id="search-link"
+        to="/search"
+        aria-label="Page Recherche"
+      >
         <h1 className="desktop-link">Rechercher</h1>
+        <img
+          className="mobile-link"
+          src={location.pathname === "/search" ? searchgreen : search}
+          alt=""
+        />
       </NavLink>
-      <NavLink to="/favorites" aria-label="Page Mes favoris">
+      <NavLink
+        className="nav-link"
+        to="/favorites"
+        aria-label="Page Mes favoris"
+      >
         <h1 className="desktop-link">Mes Favoris</h1>
-        <img className="mobile-link" src={favorite} alt="" />
+        <img
+          className="mobile-link"
+          src={location.pathname === "/favorites" ? favoritegreen : favorite}
+          alt=""
+        />
       </NavLink>
-      <NavLink to="/creation-recipe" aria-label="Page Creation de recette">
+      <NavLink
+        className="nav-link"
+        to="/creation-recipe"
+        aria-label="Page Creation de recette"
+      >
         <h1 className="desktop-link">Créer une recette</h1>
-        <img className="mobile-linky" src={add} alt="" />
+        <img
+          className="mobile-link"
+          src={location.pathname === "/creation-recipe" ? addgreen : add}
+          alt=""
+        />
       </NavLink>
-      <NavLink to="/profile" aria-label="Page Mon Profil">
+      <NavLink className="nav-link" to="/profile" aria-label="Page Mon Profil">
         <h1 className="desktop-link">Mon Profil</h1>
-        <img className="mobile-link" src={profile} alt="" />
+        <img
+          className="mobile-link"
+          src={location.pathname === "/profile" ? profilegreen : profile}
+          alt=""
+        />
       </NavLink>
     </nav>
   );
