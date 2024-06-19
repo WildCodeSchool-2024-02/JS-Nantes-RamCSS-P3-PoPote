a5aa026f9bf76a7d55469f7afa7d8f5c57e414a2
Michaël Couffignal <michael@couffignal.fr>
2024-06-19 16:52:54 +0200

    first commit, initalize login form, using existant class

--- client/src/pages/ConnexionPage.jsx
+++ client/src/pages/ConnexionPage.jsx
@@ -1,9 +1,62 @@
+import { NavLink } from "react-router-dom";
+
 function ConnexionPage() {
   return (
-    <>
-      <h1>Page Connexion</h1>
-      <h1>Page Connexion</h1>
-    </>
+
+    <section className="register-page">
+      <div className="desktop-frame">
+        <img
+          src="./src/assets/logo/logo_popote_blanc.svg"
+          alt="logo Popote"
+          className="logo-blanc"
+        />
+        <img
+          src="./src/assets/logo/logo_popote_orange.svg"
+          alt="logo Popote"
+          className="logo-orange"
+        />
+        <h2>Nous sommes heureux de vous revoir !</h2>
+
+        <section className="userform-page">
+          <form className="register-form">
+            <div className="logo-input">
+              <img
+                src="./src/assets/logo_form/icon-mail.svg"
+                alt="icon email"
+                className="icon-form"
+              />
+              <input type="email" id="login-email" placeholder="Email" required />
+            </div>
+            <div className="logo-input">
+              <img
+                src="./src/assets/logo_form/icon-lock.svg"
+                alt="icon lock"
+                className="icon-form"
+              />
+              <input
+                type="password"
+                id="login-password"
+                placeholder="Mot de passe"
+                required
+              />
+              <img
+                src="./src/assets/logo_form/icon-eye.svg"
+                alt="icon eye"
+                className="icon-form"
+              />
+            </div>
+
+            <input type="submit" id="login-submit" value="Connexion" className="submit-button" />
+          </form>
+          <p>
+            Tu n’as pas de compte ? <NavLink className="url">Inscris-toi</NavLink>
+            <br />
+            <NavLink className="url">A propos de nous</NavLink>
+          </p>
+        </section>
+      </div>
+    </section>
+
   );
 }
 
