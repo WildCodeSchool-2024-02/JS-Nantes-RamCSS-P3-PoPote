0714e68768b4779a35a19a4f31dbefc56257bcf1
Michaël Couffignal <michael@couffignal.fr>
2024-06-20 16:13:07 +0200

    US_04_connexionform desktop css finished, mobile css almost finished

--- client/src/pages/ConnexionPage.jsx
+++ client/src/pages/ConnexionPage.jsx
@@ -3,60 +3,68 @@ import { NavLink } from "react-router-dom";
 function ConnexionPage() {
   return (
     <section className="login-page">
-      <img
-        src="./src/assets/logo/logo_popote_blanc.svg"
-        alt="logo Popote"
-        className="logo-blanc"
-      />
-      <img
-        src="./src/assets/logo/logo_popote_orange.svg"
-        alt="logo Popote"
-        className="logo-orange"
-      />
-      <h2>Nous sommes heureux de vous revoir !</h2>
+      <div className="desktop-frame-connexion">
+        <img
+          src="./src/assets/logo/logo_popote_blanc.svg"
+          alt="logo Popote"
+          className="logo-blanc"
+        />
+        <img
+          src="./src/assets/logo/logo_popote_orange.svg"
+          alt="logo Popote"
+          className="logo-orange"
+        />
+        <h2>Nous sommes heureux de vous revoir !</h2>
 
-      <section className="userform-page login-userform-page">
-        <form className="register-form">
-          <div className="logo-input">
-            <img
-              src="./src/assets/logo_form/icon-mail.svg"
-              alt="icon email"
-              className="icon-form"
-            />
-            <input type="email" id="login-email" placeholder="Email" required />
-          </div>
-          <div className="logo-input">
-            <img
-              src="./src/assets/logo_form/icon-lock.svg"
-              alt="icon lock"
-              className="icon-form"
-            />
+        <section className="userform-page login-userform-page">
+          <form className="register-form">
+            <div className="logo-input">
+              <img
+                src="./src/assets/logo_form/icon-mail.svg"
+                alt="icon email"
+                className="icon-form"
+              />
+              <input
+                type="email"
+                id="login-email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="logo-input">
+              <img
+                src="./src/assets/logo_form/icon-lock.svg"
+                alt="icon lock"
+                className="icon-form"
+              />
+              <input
+                type="password"
+                id="login-password"
+                placeholder="Mot de passe"
+                required
+              />
+              <img
+                src="./src/assets/logo_form/icon-eye.svg"
+                alt="icon eye"
+                className="icon-form"
+              />
+            </div>
+
             <input
-              type="password"
-              id="login-password"
-              placeholder="Mot de passe"
-              required
+              type="submit"
+              id="login-submit"
+              value="Connexion"
+              className="submit-button"
             />
-            <img
-              src="./src/assets/logo_form/icon-eye.svg"
-              alt="icon eye"
-              className="icon-form"
-            />
-          </div>
-
-          <input
-            type="submit"
-            id="login-submit"
-            value="Connexion"
-            className="submit-button"
-          />
-        </form>
-        <p>
-          Tu n’as pas de compte ? <NavLink className="url">Inscris-toi</NavLink>
-          <br />
-          <NavLink className="url">A propos de nous</NavLink>
-        </p>
-      </section>
+          </form>
+          <p>
+            Tu n’as pas de compte ?{" "}
+            <NavLink className="url">Inscris-toi</NavLink>
+            <br />
+            <NavLink className="url">A propos de nous</NavLink>
+          </p>
+        </section>
+      </div>
     </section>
   );
 }
