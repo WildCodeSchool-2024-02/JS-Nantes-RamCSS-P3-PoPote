fd04891e1ad80089a9f1f6f296a3d1aadff0830e
TheoDegorre <degorret@gmail.com>
2024-06-26 17:24:45 +0200

    feature in progress need to review back-end strategy for detail data

--- client/src/pages/RecipePage.jsx
+++ client/src/pages/RecipePage.jsx
@@ -1,19 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import DurationElement from "../components/DurationElement";
+import NutValueElement from "../components/NutValueElement";
 
 function RecipePage() {
   const recipeDetailData = useLoaderData();
+  console.info(recipeDetailData);
 
   return (
-    <>
+    <section>
       <h1>{recipeDetailData.title}</h1>
       <img src={recipeDetailData.url_photo} alt="" />
       <h2>{recipeDetailData.people_number}</h2>
       <DurationElement duration={recipeDetailData.duration} />
+      <NutValueElement nutValue={recipeDetailData.nutValue}/>
+      <p>{recipeDetailData.step_description}</p>
 
-      <h2>{recipeDetailData.nutValue}</h2>
       <h1>Page de la recette</h1>
-    </>
+    </section>
   );
 }
 
