2e8b631bbba2571467a40a4f73f466b306739edf
Florine <florinevinet@gmail.com>
2024-06-28 14:09:35 +0200

    cgu desktop look like model

--- client/src/pages/CguPage.jsx
+++ client/src/pages/CguPage.jsx
@@ -8,11 +8,8 @@ function CguPage() {
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas totam, tempore nulla magnam voluptatibus omnis hic rerum reprehenderit repellat alias. Optio at unde debitis accusamus dolorem laborum earum praesentium sequi!
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat obcaecati in voluptatem nihil quo modi deleniti provident, vero deserunt reprehenderit doloremque est, ut laborum eos sit magni rem, maxime ratione!
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore reprehenderit in nobis recusandae optio atque beatae dolor dolorem repellat unde nostrum, explicabo dolores qui omnis reiciendis ipsa officia aspernatur sint.
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque voluptatibus praesentium itaque ut eligendi consectetur! Magni, illo animi. Sint corporis numquam quaerat natus inventore saepe vitae quam quibusdam quisquam officiis?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque culpa iure voluptates omnis dicta cumque quaerat, quis enim quasi porro numquam laboriosam fugit optio ut? Ratione culpa earum nesciunt ea?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto esse perferendis vitae ea veniam magnam, reprehenderit aut non natus? Doloremque officia deserunt at atque mollitia et suscipit ad porro odio.
       </p>
-      <NavLink to="/register" className="submit-button">Retour</NavLink>
+      <NavLink to="/register" className="cgu-button">Retour</NavLink>
       </div>
     </section>
   );
