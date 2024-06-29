60958ce4817021f3b576c1ebfe79951f4ebfd73a
Michaël Couffignal <michael@couffignal.fr>
2024-06-29 17:56:47 +0200

    adding link for about, and filling about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,9 +1,15 @@
+import team from "../assets/about/team.jpg";
+
 function AboutPage() {
   return (
-    <>
+    <div className="register-page">
       <h1>About page</h1>
-      <h1>About page</h1>
-    </>
+      <img
+        src={team}
+        className="img-team"
+        alt="Florine, Michael, Mickael et Théo, tous souriant, entrain de manger une glace"
+      />
+    </div>
   );
 }
 
