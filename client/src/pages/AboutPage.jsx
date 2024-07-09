6581475f2c3758decbf20837af6582a54dc4afd2
Michaël Couffignal <michael@couffignal.fr>
2024-07-09 18:23:59 +0200

    creat an css page for about, adding login link in about page

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,12 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 function AboutPage() {
   return (
-    <div className="register-page">
-      <h1>About page</h1>
+    <div className="about-page">
+      <h2>L'équipe de Po'pote</h2>
+      <p>Florine, Michaël, Mickaël, Théo</p>
       <img
         src={`${import.meta.env.VITE_API_URL}/about/team.jpg`}
-        className="img-team"
-        alt="Florine, Michael, Mickael et Théo, tous souriant, entrain de manger une glace"
+        alt="Florine, Michaël, Mickaël et Théo, tous souriant, entrain de manger une glace"
       />
+      <nav className="about-nav">
+        <NavLink to="/connexion">Connexion</NavLink>
+      </nav>
     </div>
   );
 }
