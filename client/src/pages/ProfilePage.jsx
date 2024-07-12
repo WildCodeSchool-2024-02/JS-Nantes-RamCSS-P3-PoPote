cad4f1ceff7d57414f4ce04554b4e68a61a15c68
Florine <florinevinet@gmail.com>
2024-07-12 12:19:19 +0200

    bloc name visible on profile

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -5,17 +5,31 @@ function ProfilePage() {
   return (
     <section className="my-profile">
       <h1>Mon compte</h1>
-      <div>
-      <img
+      <div className="bloc-name">
+        <img
           id="profile-picture-img"
           src={`${import.meta.env.VITE_API_URL}/profile/sophie-nancier.jpg`}
           alt="profil de l'utilisateur"
         />
-        <p>{firstname} {lastname}</p>
-        {/* <img />
-        <img /> */}
+        <p>
+          {firstname} {lastname}
+        </p>
+        <div className="profile-picto">
+          <img
+            id="dots-img"
+            src={`${import.meta.env.VITE_API_URL}/profile/dots.svg`}
+            alt="dots to go to parameters"
+          />
+          <img
+            id="pen-modification-img"
+            src={`${import.meta.env.VITE_API_URL}/profile/pen-modification.svg`}
+            alt="pen to modify profile"
+          />
+        </div>
       </div>
-      <button type="submit">DECONNEXION</button>
+      <button type="submit" className="deconnexion-button">
+        DECONNEXION
+      </button>
       <h2>Mes recettes créées</h2>
     </section>
   );
