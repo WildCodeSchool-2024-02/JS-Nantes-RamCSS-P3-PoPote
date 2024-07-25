272323ae6e57e3a4782c446fa0333aa5b9b0e175
Michaël Couffignal <michael@couffignal.fr>
2024-07-25 09:44:13 +0200

    add modal for admin in profile page, acces only if admin, can delete any recipe

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,16 +1,19 @@
 import { useLoaderData } from "react-router-dom";
 import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 import MaxiRecipeCard from "../components/MaxiRecipeCard";
 import ProfileModal from "../components/ProfileModal";
-import { AuthContext } from "../context/AuthContext";
+import ProfileModalAdmin from "../components/AdminModal";
+
 
 function ProfilePage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalAdminOpen, setIsModalAdminOpen] = useState(false);
   const {user} = useContext(AuthContext);
 
   const id = localStorage.getItem("userId");
+  const Admin = localStorage.getItem("isAdmin");
   const recipeData = useLoaderData();
-
   function openModal() {
     setIsModalOpen(true);
   }
@@ -18,6 +21,46 @@ function ProfilePage() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  function openAdminModal() {
+    setIsModalAdminOpen(true);
+  }
+
+  const closeAdminModal = () => {
+    setIsModalAdminOpen(false);
+  };
+  let modalContent;
+  if (isModalOpen) {
+    modalContent = <ProfileModal closeModal={closeModal} id={id} />;
+  } else if (isModalAdminOpen) {
+    modalContent = <ProfileModalAdmin closeAdminModal={closeAdminModal} />;
+  } else {
+    modalContent = (
+      <>
+        <button type="submit" className="deconnexion-button">
+          DECONNEXION
+        </button>
+        <h2>Mes recettes créées</h2>
+        <article className="recipe-card-list">
+          {recipeData.length > 0 ? (
+            recipeData.map((el) => (
+              <MaxiRecipeCard
+                key={el.id}
+                id={el.id}
+                title={el.title}
+                photo={el.url_photo}
+                duration={el.duration}
+                nutValue={el.nutValue}
+                className="recipe-card"
+              />
+            ))
+          ) : (
+            <p>Vous n'avez pas encore créé de recettes</p>
+          )}
+        </article>
+      </>
+    );
+  }
 
   return (
     <section className="my-profile">
@@ -46,35 +89,18 @@ function ProfilePage() {
               alt="pen to modify profile"
             />
           </button>
+          {Admin === "1" && (
+            <button type="button" onClick={openAdminModal}>
+              <img
+                id="admin"
+                src={`${import.meta.env.VITE_API_URL}/profile/profile_admin_icon.svg`}
+                alt="Shield icon, to go modify recipe"
+              />
+            </button>
+          )}
         </div>
       </div>
-      {isModalOpen ? (
-        <ProfileModal closeModal={closeModal} id={id} />
-      ) : (
-        <>
-          <button type="submit" className="deconnexion-button">
-            DECONNEXION
-          </button>
-          <h2>Mes recettes créées</h2>
-          <article className="recipe-card-list">
-            {recipeData.length > 0 ? (
-              recipeData.map((el) => (
-                <MaxiRecipeCard
-                  key={el.id}
-                  id={el.id}
-                  title={el.title}
-                  photo={el.url_photo}
-                  duration={el.duration}
-                  nutValue={el.nutValue}
-                  className="recipe-card"
-                />
-              ))
-            ) : (
-              <p>Vous n'avez pas encore créé de recettes</p>
-            )}
-          </article>
-        </>
-      )}
+      {modalContent}
     </section>
   );
 }
